2aba469fe8541a16b5f1f224debaf28db414a26f
erisinger <erisinger@umass.edu>
2017-01-26 23:02:14 -0500

    first commit

---
+++ app/src/main/java/edu/umass/cs/sensors/surveytest/DummySurvey.java
@@ -0,0 +1,53 @@
+package edu.umass.cs.sensors.surveytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.JsonWriter;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DummySurvey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_survey);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        JSONObject surveyJSON;
+        String prettyString;
+        String surveyString = getIntent().getStringExtra(MainActivity.ESCAPED_SURVEY);
+
+        System.out.println(surveyString);
+        try {
+            surveyJSON = new JSONObject(surveyString + "\n");
+            prettyString = surveyJSON.toString(2);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            prettyString = "JSON error";
+        }
+
+        TextView surveyTextView = (TextView)findViewById(R.id.survey_text);
+        surveyTextView.setText(prettyString);
+    }
+
+    public void sendSurveyResponse(View view) {
+        Intent response = getIntent();
+        String escapedResponse = getResources().getString(R.string.survey_result_escaped);
+        String unescapedResponse = getResources().getString(R.string.survey_result_unescaped);
+        response.putExtra(MainActivity.ESCAPED_RESPONSE, escapedResponse);
+        response.putExtra(MainActivity.UNESCAPED_RESPONSE, unescapedResponse);
+        setResult(MainActivity.SUCCESSFUL_SURVEY_RESPONSE, response);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/edu/umass/cs/sensors/surveytest/MainActivity.java
@@ -0,0 +1,60 @@
+package edu.umass.cs.sensors.surveytest;
+
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String DISPLAY_SURVEY = "display";
+    public static String ESCAPED_SURVEY = "survey-escaped";
+    public static String UNESCAPED_SURVEY = "survey-unescaped";
+    public static String ESCAPED_RESPONSE = "response-escaped";
+    public static String UNESCAPED_RESPONSE = "response-unescaped";
+    public static int SURVEY_RESULT = 1;
+    public static int SUCCESSFUL_SURVEY_RESPONSE = 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openSurvey(View view) {
+        Intent surveyIntent = new Intent(this, DummySurvey.class);
+        surveyIntent.setAction(DISPLAY_SURVEY);
+        surveyIntent.putExtra(UNESCAPED_SURVEY, getResources().getString(R.string.survey_JSON_unescaped));
+        surveyIntent.putExtra(ESCAPED_SURVEY, getResources().getString(R.string.survey_JSON_escaped));
+        startActivityForResult(surveyIntent, SURVEY_RESULT);
+
+//        System.out.println(R.string.survey_JSON_escaped);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SURVEY_RESULT) {
+            if (resultCode == SUCCESSFUL_SURVEY_RESPONSE) {
+                String response = data.getStringExtra(ESCAPED_RESPONSE);
+                try {
+                    String prettyResponse = new JSONObject(response).toString(2);
+                    Toast toast = Toast.makeText(this, prettyResponse, Toast.LENGTH_SHORT);
+                    toast.show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d("result code", "" + resultCode);
+            }
+        }
+    }
+}
